61cb834e87967b1af514258fd5761fe950db66c5
Wathiq Al-Qajar <wathiq-iq@users.noreply.github.com>
2016-05-15 22:05:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -20,6 +20,8 @@
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
+<!--     CEO     -->
+<link rel="alternate" href="http://lazubot.github.io/Lazu/" hreflang="en-us" />
 
 <style type="text/css">#website h1,#website h2,#website #content h3,#website .topmenu{font-family:"Droid Sans","Helvetica Neue",Helvetica,Arial,sans-serif}#website,#website p{font-family:"Varela Round","Helvetica Neue",Helvetica,Arial,sans-serif}.container_content{}</style>
 
